7b205757f8b9a0c74139bc754941c41f737732f5
Vadim Zaytsev <vadim@grammarware.net>
2015-06-27 14:33:43 +0200

    Cleanup

--- legacy/newvenues.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python3
-
-f = open('venues.unq','r')
-g = open('venues.lst','w')
-for line in f.readlines():
-	line = line.strip().replace("'","\\'")
-	g.write("'%s':\n\t'%s',\n" % (line, line.split('(')[0].strip()))
-f.close()
-g.close()
