aeee77188673666c26bd03516db4e4ef48eaabd4
Vadim Zaytsev <vadim@raincode.com>
2018-10-19 13:25:09 +0200

    add a C# sketch attempt

---
+++ XFit/XFit/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace XFit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Write_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Quit_Click(object sender, RoutedEventArgs e)
+            => this.Close();
+    }
+}
