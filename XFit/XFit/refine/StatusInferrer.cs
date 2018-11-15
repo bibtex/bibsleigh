de1e3810671ff6b25a12eca914ea273bbc4e3a8e
Vadim Zaytsev <vadim@raincode.com>
2018-11-15 09:03:23 +0100

    refine status further

--- XFit/XFit/refine/StatusInferrer.cs
+++ XFit/XFit/refine/StatusInferrer.cs
@@ -57,6 +57,7 @@ namespace XFit.refine
             Xformation action;
             switch (brak.ToLower())
             {
+                case "s": // happens in SEKE (probably corrupt metadata)
                 case "full paper":
                     action = new XPaper(GenPaperAction(main));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
@@ -65,10 +66,13 @@ namespace XFit.refine
                 case "abstract for talk":
                 case "abstract only":
                 case "talk abstract":
+                case "detailed abstract":
                 case "extended abstract":
+                case "extended abstracts":
                 case "extende abstract": // misspelling!
                 case "extended summary":
                 case "research summary":
+                case "extended outline":
                 case "summary":
                 case "summaray": // misspelling!
                 case "a summary":
@@ -84,6 +88,7 @@ namespace XFit.refine
                     break;
                 case "an experience report":
                 case "experience report":
+                case "experience paper":
                 case "status report":
                 case "partial report":
                     action = new XPaper(GenPaperAction(main, "report"));
@@ -96,9 +101,11 @@ namespace XFit.refine
                 case "keynote address":
                 case "keynote paper":
                 case "keynote talk":
+                case "seip keynote": // special case
                     action = new XPaper(GenPaperAction(main, "keynote"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
+                case "industrial session":
                 case "industrial paper":
                 case "industrial talk":
                     action = new XPaper(GenPaperAction(main, "industrial"));
@@ -115,10 +122,19 @@ namespace XFit.refine
                     action = new XPaper(GenPaperAction(main, "invited"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
+                case "abstract of invited tutorial":
+                    action = new XPaper(GenPaperAction(main, "abstract", "invited", "tutorial"));
+                    Manager.RegisterAction(ACTION_NAME, paper, action);
+                    break;
                 case "invited tutorial":
                     action = new XPaper(GenPaperAction(main, "invited", "tutorial"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
+                case "tutorial/keynote":
+                    action = new XPaper(GenPaperAction(main, "keynote", "tutorial"));
+                    Manager.RegisterAction(ACTION_NAME, paper, action);
+                    break;
+                case "invited talk abstract":
                 case "invited talk, abstract only":
                 case "abstract of an invited lecture":
                 case "abstract of invited lecture":
@@ -140,8 +156,10 @@ namespace XFit.refine
                     action = new XPaper(GenPaperAction(main, "short"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
+                case "system descrition": // misspelling!
                 case "system description":
                 case "system descriptions":
+                case "system exhibition":
                     action = new XPaper(GenPaperAction(main, "system description"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
@@ -153,22 +171,39 @@ namespace XFit.refine
                     action = new XPaper(GenPaperAction(main, "tool"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
+                case "demo abstract":
+                    action = new XPaper(GenPaperAction(main, "tool", "abstract"));
+                    Manager.RegisterAction(ACTION_NAME, paper, action);
+                    break;
                 case "mini-tutorial":
+                case "distilled tutorial":
+                case "a tutorial":
                 case "tutorial":
                 case "tutorial session":
                     action = new XPaper(GenPaperAction(main, "tutorial"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
+                case "position statement":
+                case "position paper":
                 case "vision paper":
                     action = new XPaper(GenPaperAction(main, "vision"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
+                case "functional pearl":
+                case "declarative pearl":
+                    action = new XPaper(GenPaperAction(main, "pearl"));
+                    Manager.RegisterAction(ACTION_NAME, paper, action);
+                    break;
+                case "a preliminary report":
                 case "preliminary report":
+                case "preliminary draft":
                 case "preliminary version":
                 case "work in progress":
+                case "working paper":
                     action = new XPaper(GenPaperAction(main, "work in progress"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
                     break;
+                case "preliminary abstract":
                 case "preliminary extended abstract":
                     action = new XPaper(GenPaperAction(main, "abstract", "work in progress"));
                     Manager.RegisterAction(ACTION_NAME, paper, action);
@@ -207,6 +242,17 @@ namespace XFit.refine
                 p2.title = newtitle;
                 p2.AddStatus(status1);
                 p2.AddStatus(status2);
+                return p2;
+            };
+
+        private Func<Paper, Paper> GenPaperAction(string newtitle, string status1, string status2, string status3)
+            => p =>
+            {
+                var p2 = Parser.Clone(p);
+                p2.title = newtitle;
+                p2.AddStatus(status1);
+                p2.AddStatus(status2);
+                p2.AddStatus(status3);
                 return p2;
             };
     }
